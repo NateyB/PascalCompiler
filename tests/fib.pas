99b585458de21ea07743e249c40f5cf11fa98fe5
NateyB <spamcollectordelta@gmail.com>
2016-08-23 22:28:23 -0500

    Completed the test program using all of the LALR(1) Pascal grammar subset by adding an additional array structure.
    I found some interesting language concepts (dynamically allocated array lengths, for example) which are not included in the subset.

--- tests/fib.pas
+++ tests/fib.pas
@@ -1,10 +1,14 @@
 program fib(input, output);
-var n: integer;
+var n, p: integer;
 var q: real;
+var useless_array : array [1..12] of integer;
+  (*Strangely, the above type can simply be "array of integer", and the
+  "setLength" function can be used letter to define its length. I wonder why
+  this syntactical construct wasn't included.*)
 
-function fib(a, b, c : integer) : integer;
+function fib(a : integer; b, c : real) : real;
 begin
-    if a = 0 then fib := c
+    if a <= 1 then fib := c
     else fib := fib(a - 1, c, b + c)
 end;
 
@@ -15,7 +19,7 @@ begin
   b := 0;
   sum := 1;
   c := b;
-  while (a > 0) do
+  while (not a < -1) do
     begin
       a := a - 1;
       b := sum;
@@ -25,13 +29,19 @@ begin
   fib2 := sum;
 end;
 
-function init() : integer;
+procedure init();
 begin
   n := 12;
+  p := 12;
+  useless_array[3] := 15;
+(* setLength(memoization, n);
+  Strangely, this feature only works if the data above is changed. Hmm.*)
   q := 12
 end;
 
 begin
-    init();
-    writeln(fib2(n)*q)
+    init;
+    writeln(+fib(n, 0, 1)*q/p + 4);
+    writeln(fib2(n));
+    writeln(useless_array[3])
 end.
