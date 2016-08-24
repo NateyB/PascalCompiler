9239cc864822331a24608287f8fd0e866edf4c8d
NateyB <spamcollectordelta@gmail.com>
2016-08-23 21:41:27 -0500

    Added an iterative version of the fiboanacci sequence (using a while loop).

--- tests/fib.pas
+++ tests/fib.pas
@@ -1,13 +1,37 @@
 program fib(input, output);
 var n: integer;
+var q: real;
 
-function fib(a, b, c, d : integer) : integer;
+function fib(a, b, c : integer) : integer;
 begin
     if a = 0 then fib := c
-    else fib := fib(a - 1, c, b + c, d + b + c)
+    else fib := fib(a - 1, c, b + c)
+end;
+
+function fib2(a : integer) : integer;
+var b, c, sum : integer;
+begin
+  a := a - 1;
+  b := 0;
+  sum := 1;
+  c := b;
+  while (a > 0) do
+    begin
+      a := a - 1;
+      b := sum;
+      sum := c + sum;
+      c := b
+    end;
+  fib2 := sum;
+end;
+
+function init() : integer;
+begin
+  n := 12;
+  q := 12
 end;
 
 begin
-    read(n);
-    writeln(fib(n - 1, 0, 1, 0))
+    init();
+    writeln(fib2(n)*q)
 end.
