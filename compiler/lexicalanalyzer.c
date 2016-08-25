a7e6bc0dfb61e2eed8dea69bb00ffeca70f332b9
NateyB <spamcollectordelta@gmail.com>
2016-08-25 01:22:35 -0500

    Line numbers and the file are now printed out! Now to write it to a file.

--- compiler/lexicalanalyzer.c
+++ compiler/lexicalanalyzer.c
@@ -3,7 +3,7 @@
 int fib(int n) {
     int sum = 0;
     int last = 1;
-    int placeholder;
+    int placeholder = 0;
     for (int i = 0; i < n; i++) {
         placeholder = sum;
         sum += last;
@@ -12,8 +12,30 @@ int fib(int n) {
     return sum;
 }
 
+void copyFile(FILE *sourceFile) {
+    int ln = 1;
+    int c = 0;
+    while ((c = fgetc(sourceFile)) != EOF) {
+        printf("%c", c);
+        if (c == '\n')
+        {
+            printf("%6d    ", ln++);
+        }
+    }
+}
+
 int main() {
-    printf("%s\n", "Love ya!");
+    FILE *sourceFile = fopen("tests/fib.pas", "r");
+    if (sourceFile != NULL)
+    {
+        copyFile(sourceFile);
+        fclose(sourceFile);
+    } else
+    {
+        printf("%s\n", "It was null?");
+    }
+    printf("\n");
+
     printf("%d\n", fib(12));
     return 0;
 }
