3a3a54e0fa52d99a8bbb7315b9d2479c5fb97cf2
NateyB <spamcollectordelta@gmail.com>
2016-09-02 11:31:56 -0500

    Created a struct that contains the information that should be passed to the listing file. Separated out the code better, for greater modularizability. Still haven't started implementing machines, but am keeping that in mind while testing.

--- compiler/lexicalanalyzer.c
+++ compiler/lexicalanalyzer.c
@@ -1,30 +1,68 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-// This function outputs a copy of the source file with line numbers to the
-// output file.
-void copyFile(FILE *sourceFile, FILE *outputFile) {
-    int ln = 1;
-    int c = 0;
-    char buffer[72] = {0};
+enum ListingType {LNNUM, ERROR};
 
-    while (fgets(buffer, 73, sourceFile)) {
-        fprintf(outputFile, "%6d    ", ln++);
-        fprintf(outputFile, "%s", buffer);
+struct Listing {
+    enum ListingType type;
+    char message[72];
+
+    union {
+        int ln;
+        char error[7];
+    };
+};
+
+void outToListing(struct Listing* entry, FILE *listingFile) {
+    switch (entry -> type)
+    {
+        case LNNUM:
+            fprintf(listingFile, "%7d   %s", entry -> ln, entry -> message);
+            break;
+        case ERROR:
+            fprintf(listingFile, "%7s   %s", entry -> error, entry -> message);
+            break;
     }
 }
 
-int main() {
+// This function outputs a copy of the source file with line numbers to the
+// output file.
+void copyFile(FILE *sourceFile, FILE *listingFile) {
+    int ln = 1;
+    int c = 0;
+    struct Listing* list = malloc(sizeof(*list));
+
+    while (fgets(list->message, 73, sourceFile))
+    {
+        list->ln++;
+        outToListing(list, listingFile);
+    }
+
+}
+
+// Returns 0 on failure, 1 on success.
+int init() {
     FILE *sourceFile = fopen("tests/fib.pas", "r");
-    FILE *outputFile = fopen("out/listing.txt", "w+");
+    FILE *listingFile = fopen("out/listing.txt", "w+");
     if (sourceFile != NULL)
     {
-        copyFile(sourceFile, outputFile);
+        copyFile(sourceFile, listingFile);
         fclose(sourceFile);
     } else
     {
         printf("%s\n", "It was null?");
+        return 0;
     }
-    fclose(outputFile);
+    fclose(listingFile);
 
+    return 1;
+}
+
+int main() {
+    if (init()) {
+
+    } else {
+        return 1;
+    }
     return 0;
 }
